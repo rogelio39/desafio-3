fba6b1451927dc3240386bda2bec522cbdf69340
Rogelio Suleta <andresrogesu@gmail.com>
2023-08-24 17:19:25 -0300

    se hicieron las correcciones, falta agregar el manejador de carrito

--- src/app.js
+++ src/app.js
@@ -1,112 +1,48 @@
 import express from "express";
-import { ProductsManager } from "./models/productsManager.js";
-import { Products } from "./models/Products.js";
+import { __dirname } from "./path.js";
+import { engine } from "express-handlebars";
+import path from 'path';
+import multer from "multer";
+
+//rutas
+import prodsRouter from "./routes/products.routes.js";
 
 const PORT = 4000;
 
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
-const productManager = new ProductsManager(); // Crear una única instancia de ProductsManager
+//config multer
+const storage = multer.diskStorage({
+    destination: (req, file, callback) => {
+        callback(null, 'src/public/img');
+    },
+    filename: (req, file, callback) => {
+        callback(null, `${Date.now()} ${file.originalname}`)
+    }
+});
+
+const upload = multer({ storage: storage });
 
 
-function addProduct(prod) {
-    const { title, description, price, code, stock, thumbnail, id } = prod;
-    const newProduct = new Products(title, description, price, code, stock, thumbnail, id);
-    productManager.addProduct(newProduct);
-
-
-}
-
+//routes
+app.use('/api/products', prodsRouter);
+app.use('/static', express.static(path.join(__dirname, '/public')));//aqui se deben concatenar las rutas.
 
 
 app.get('/', (req, res) => {
     res.send('inicio')
-})
-
-app.get('/products', async (req, res) => {
-    try {
-        const products = await productManager.getProducts();
-        const { limit } = req.query;
-        if (limit) {
-            const prod = products.slice(0, limit);
-            res.status(200).send(prod);
-        } else {
-            res.status(200).send(products);
-        }
-    } catch (error) {
-        res.status(500).send('error al cargar productos', error);
-    }
-})
-
-app.get('/products/:id', async (req, res) => {
-    try {
-        const products = await productManager.getProducts();
-        const prod = products.find((product) => product.id === parseInt(req.params.id));
-
-        if (prod) {
-            res.status(200).send(prod);
-        } else {
-            res.status(404).send('Producto no existente');
-        }
-    } catch (error) {
-        res.status(500).send('error', error);
-    }
 });
 
-app.post('/products', async (req, res) => {
-    try {
-        const products = await productManager.getProducts();
-        const prod = products.find((product) => product.code === req.body.code);
-
-        if (prod) {
-            res.status(400).send("producto existente");
-        } else {
-            addProduct(req.body);
-            res.status(200).send(products);
-        }
-    } catch (error) {
-        res.status(500).send('error', error);
-    }
-});
-
-app.put('/products/:id', async (req, res) => {
-    const { id } = req.params;
-    const { title, description, price, code, stock, thumbnail } = req.body;
-    const products = await productManager.getProducts();
-
-    const productToUpdate = products.find(prod => prod.id === parseInt(id));
-    if (productToUpdate) {
-        productManager.updatedProduct(parseInt(id), 'title', title);
-        productManager.updatedProduct(parseInt(id), 'description', description);
-        productManager.updatedProduct(parseInt(id), 'price', price);
-        productManager.updatedProduct(parseInt(id), 'code', code);
-        productManager.updatedProduct(parseInt(id), 'stock', stock);
-        productManager.updatedProduct(parseInt(id), 'thumbnail', thumbnail);
-        res.status(200).send(`producto ${title} actualizado`);
-    } else {
-        res.status(404).send('producto no existente');
-    }
-
+//este es el endpoint en el que me voy a conectar a mi aplicacion
+app.post('/upload', upload.single('product'), (req, res) =>{
+    res.status(200).send('imagen cargada');
 })
 
 
-app.delete('/products/:id', async (req, res) => {
-    const { id } = req.params;
-    const products = await productManager.getProducts();
-
-    const productToDelete = products.find(prod => prod.id === parseInt(id));
-    if (productToDelete) {
-        productManager.deleteProduct(parseInt(id));
-        res.status(200).send(`producto eliminado`);
-    } else {
-        res.status(404).send('producto no existente');
-    }
 
 
-}
-)
 
 app.listen(PORT, () => {
     console.log(`server on port ${PORT}`);
---
+++ src/routes/products.routes.js
@@ -0,0 +1,110 @@
+import {Router} from "express";
+import { ProductsManager } from "../models/productsManager.js";
+import { Products } from "../models/products.js";
+
+const productManager = new ProductsManager();
+
+async function addProduct(prod) {
+    const { title, description, price, code, stock, thumbnail } = prod;
+    const newProduct = new Products(title, description, price, code, stock, thumbnail);
+    await productManager.addProduct(newProduct);
+    return newProduct;
+}
+
+
+const prodsRouter = Router();
+
+
+prodsRouter.get('/', async (req, res) => {
+    try {
+        const products = await productManager.getProducts();
+        const { limit } = req.query;
+        if (limit) {
+            const prod = products.slice(0, limit);
+            res.status(200).send(prod);
+        } else {
+            res.status(200).send(products);
+        }
+    } catch (error) {
+        res.status(500).send('error al cargar productos', error);
+    }
+})  
+
+
+
+prodsRouter.get('/:id', async (req, res) => {
+    try {
+        const {id} = req.params;
+        const product = await productManager.getProductById(parseInt(id));
+
+        if (product) {
+            res.status(200).send(product);
+        } else {
+            res.status(404).send('Producto no existente');
+        }
+    } catch (error) {
+        res.status(500).send('error', error);
+    }
+});
+
+
+prodsRouter.post('/', async (req, res) => {
+    try {
+        const products = await productManager.getProducts();
+        const prod = products.find((product) => product.code === req.body.code);
+
+        if (prod) {
+            res.status(400).send("producto existente");
+        } else {
+            await addProduct(req.body);
+            res.status(200).send('producto creado con exito');
+
+        }
+    } catch (error) {
+        res.status(500).send('error', error);
+    }
+});
+
+
+prodsRouter.put('/:id', async (req, res) => {
+    const { id } = req.params;
+    const { title, description, price, code, stock, thumbnail } = req.body;
+    const products = await productManager.getProducts();
+
+    const productToUpdate = products.find(prod => prod.id === parseInt(id));
+    if (productToUpdate) {
+        productManager.updatedProduct(parseInt(id), 'title', title);
+        productManager.updatedProduct(parseInt(id), 'description', description);
+        productManager.updatedProduct(parseInt(id), 'price', price);
+        productManager.updatedProduct(parseInt(id), 'code', code);
+        productManager.updatedProduct(parseInt(id), 'stock', stock);
+        productManager.updatedProduct(parseInt(id), 'thumbnail', thumbnail);
+        res.status(200).send(`producto ${title} actualizado`);
+    } else {
+        res.status(404).send('producto no existente');
+    }
+
+});
+
+
+prodsRouter.delete('/:id', async (req, res) => {
+    const { id } = req.params;
+    const products = await productManager.getProducts();
+
+    const productToDelete = products.find(prod => prod.id === parseInt(id));
+    if (productToDelete) {
+        productManager.deleteProduct(parseInt(id));
+        res.status(200).send(`producto eliminado`);
+    } else {
+        res.status(404).send('producto no existente');
+    }
+
+
+}
+);
+
+
+
+
+
+export default prodsRouter;
