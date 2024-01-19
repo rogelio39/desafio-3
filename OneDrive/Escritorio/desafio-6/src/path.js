dcca9da9cce3141ed53b83ef1091dd44ce576df6
Rogelio Suleta <andresrogesu@gmail.com>
2024-01-19 19:46:20 -0300

    archivos eliminados

--- OneDrive/Escritorio/desafio-6/src/path.js
+++
@@ -1,6 +0,0 @@
-import {fileURLToPath} from "url"; 
-import {dirname} from "path";
-
-const __filename = fileURLToPath(import.meta.url);
-//esto devuelve el path del archivo
-export const __dirname = dirname(__filename); 
